a10ef8f724468a5c6f97661a6cb1cce08bab1a5b
Mutesi <ktesialine@gmail.com>
2023-02-27 12:02:37 +0200

    feat:hero section

---
+++ components/sections/Hero.jsx
@@ -0,0 +1,35 @@
+"use client";
+import Image from "next/image";
+import Data from "data/HeroData";
+import { useState } from "react";
+export default function Hero() {
+  const [img, setImage] = useState(0);
+  function getImageForward(){
+   setImage((prevImg) => prevImg + 1);    
+  }
+   function getImageBackWard() {
+    setImage((prevImg) => prevImg - 1);
+  
+   }
+  return (
+    <div className="w-full relative">
+      <div className="flex relative">
+        <Image
+          src={Data[img].pic}
+          alt={Data[img].name}
+          width={Data[img].width}
+          height={Data[img].height}
+          className="w-full"
+        />
+      </div>
+      <div className="bg-black text-white py-4 px-6 w-[500px] absolute -bottom-10 left-10">
+        <h2>Join us in supporting Ukraine’s university students</h2>
+        <p>Donations help them continue their studies during the crisis</p>
+        <div className="flex gap-x-8 px-16">
+          <button onClick={getImageBackWard}>Backward</button>
+          <button onClick={getImageForward} className="p-4">forward </button>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ data/HeroData.js
@@ -0,0 +1,16 @@
+
+const Data = [
+  {
+    name : "student",
+     pic : "/images/Aalto_14_6_19_0006.webp" ,
+     width:1367,
+     height: 500
+  },
+ {
+  name : "school",
+ pic : "/images/Student_Guide_Use_Only_Uusille opiskelijoille_Wide copy.webp",
+   width :1362,
+     height: 500
+ }
+];
+export default Data;
